e84a43177564c8f67ae0541a81fb3cd7a4b48c61
marisol-001 <58320037+marisol-001@users.noreply.github.com>
2019-12-03 08:39:56 -0500

    se agrego el ejercicio 7

---
+++ ejercicio7.py
@@ -0,0 +1,11 @@
+#ejercicio7
+#declarar
+tiempodesalida,tiempodellegada,diferenciadetiempo=0,0,0
+tiempodesalida=int(input("tiempo de salida"))
+tiempodellegada=int(input("tiempo de llegada"))
+diferenciadetiempo=tiempodesalida-tiempodellegada
+
+#procesing
+if(diferenciadetiempo<8):
+    print("velocidad excelente")
+#fin-if
