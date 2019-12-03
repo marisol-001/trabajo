8ef8ed9089cef39ba5157fe5265b3f09b12742ff
marisol-001 <58320037+marisol-001@users.noreply.github.com>
2019-12-03 17:29:05 -0500

    se agrego el ejercicio 22

---
+++ ejercicio22.py
@@ -0,0 +1,18 @@
+# exceso de alumnos en un aula
+#declaracion
+institucion=""
+grado:""
+seccion=""
+numero_alumnos=0
+
+#Input
+institucion=input("ingrese institucion:")
+grado=input("ingrese grado:")
+seccion=input("ingrese acciones:")
+numero_alumnos=int(input("ingrese numero de alumnos:"))
+
+#procesing
+total=(numero_alumnos)
+if(total>30):
+    print(exceso_alumnos_en_aula)
+#fin_if
