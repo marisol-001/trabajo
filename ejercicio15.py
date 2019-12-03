cb591ac9b6c4b712512b3b0ed387a145f315858e
marisol-001 <58320037+marisol-001@users.noreply.github.com>
2019-12-03 16:27:59 -0500

    se agrego el ejercicio 15

---
+++ ejercicio15.py
@@ -0,0 +1,14 @@
+#ejercicio15
+#declarar
+base,altura,areadeltriangulo=0.0,0.0,0.0
+
+base=float(input("ingrese base"))
+altura=float(input("ingrese altura"))
+
+areadeltriangulo=(base*altura)/2
+
+#processing
+if(areadeltriangulo>60):
+    print("triangulo grande")
+#fin_if
+
