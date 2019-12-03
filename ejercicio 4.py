b74495c575e4a5fb687a4740ae349f4e04b01b4f
marisol-001 <58320037+marisol-001@users.noreply.github.com>
2019-12-03 08:27:06 -0500

    se agrego el ejercicio 4

---
+++ ejercicio 4.py
@@ -0,0 +1,10 @@
+#ejercicio4
+#declarar
+mensajesenviados=0
+mensajesenviados=int(input("mensajes enviados"))
+#procesing
+if(mensajesenviados>500):
+    print("se le cobrara 1.50 por cada mesaje")
+#fin-if
+
+
