26c16b11fa66033c5048daad4157180af3cf5ad3
marisol-001 <58320037+marisol-001@users.noreply.github.com>
2019-12-03 08:33:34 -0500

    se agrego el ejercicio 6

---
+++ ejercicio6.py
@@ -0,0 +1,12 @@
+#ejercicio6
+#declarar
+preciodecompra,preciodeventa,ganancia=0,0,0
+preciodecompra=int(input("precio de compra"))
+preciodeventa=int(input("precio de venta"))
+ganancia=preciodeventa-preciodecompra
+
+#procesing
+if(ganancia>500):
+    print("excelente venta")
+#fin-if
+
