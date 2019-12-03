54203510763cb6bb9ca5ae52c22ae56ce37f7348
marisol-001 <58320037+marisol-001@users.noreply.github.com>
2019-12-03 16:01:50 -0500

    se agrego el ejercicio 14

---
+++ ejercicio14.py
@@ -0,0 +1,14 @@
+#ejercicio14
+#declarar
+lado1,lado2,area=0.0,0.0,0.0
+lado1=float(input("ingrese lado 1 "))
+lado2=float(input("ingrese lado 2"))
+
+area=lado1*lado2
+
+#processing
+if(area>60):
+    print("cuadrado grande")
+else:
+    print("cuadrado pequeño")
+#fin_if
