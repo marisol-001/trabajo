29be0dd9ed218dfcdaec4564e4124e0283b93ce0
marisol-001 <58320037+marisol-001@users.noreply.github.com>
2019-12-03 17:28:15 -0500

    se agrego el ejercicio 21

---
+++ ejercicio21.py
@@ -0,0 +1,16 @@
+# socio mayoritario
+#declaracion
+nombre=""
+ruc:""
+acciones=0
+
+#Input
+nombre=input("ingrese nombre:")
+ruc=input("ingrese ruc:")
+acciones=int(input("ingrese acciones:"))
+
+#procesing
+total=(acciones)
+if(total>10):
+    print("socio_mayoritario")
+#fin_if
