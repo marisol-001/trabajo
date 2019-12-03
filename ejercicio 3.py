011527f34d1dd28e40654b523eee0899e51e2bf5
marisol-001 <58320037+marisol-001@users.noreply.github.com>
2019-12-03 07:52:20 -0500

    se agrego el ejercicio 3

---
+++ ejercicio 3.py
@@ -0,0 +1,13 @@
+#ejercicio 3
+#declarar
+talla1,talla2,tallafinal=0,0,0
+talla1=int(input("talla1"))
+talla2=int(input("talla2"))
+
+tallafinal=talla1-talla2
+#preocesing
+if(tallafinal<7):
+    print("tratamiento bueno")
+#fin-if
+
+
