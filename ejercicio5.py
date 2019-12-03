30987689c51f274ed3a8c5aad48544170d21051c
marisol-001 <58320037+marisol-001@users.noreply.github.com>
2019-12-03 08:28:04 -0500

    se agrego el ejercicio 5

---
+++ ejercicio5.py
@@ -0,0 +1,11 @@
+#ejercicio6
+#declarar
+numerodeentradas=0
+numerodeentradas=int(input("numero de entradas"))
+
+#procesing
+if(numerodeentradas>50):
+    print("grat0is 2 entradas")
+else:
+    print("no ganaste")
+#fin-if
