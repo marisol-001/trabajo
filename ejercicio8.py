6668a04f54fae566b7de55d1994b2719c98f5be6
marisol-001 <58320037+marisol-001@users.noreply.github.com>
2019-12-03 08:53:37 -0500

    se agrego el ejercicio 8

---
+++ ejercicio8.py
@@ -0,0 +1,10 @@
+#ejercicio8
+#declarar
+tipodeempleado=""
+publico=""
+tipodeempleado=str(input("tipo de empleado"))
+#procesing
+if(tipodeempleado>"publico"):
+    print("aplicar un descuento de 20% al monto")
+#fin-ifp
+
